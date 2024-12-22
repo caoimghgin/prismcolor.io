7fa84bcb0c68e14bc0b4b7bf29484b6acf70fcb2
Frank Stallone <fstallone@strongtie.com>
2024-12-22 08:45:57 -0500

    feat: Adding framer motion to Swatches in the SwatchGroup

--- components/SwatchGroup/SwatchGroup.js
+++ components/SwatchGroup/SwatchGroup.js
@@ -1,16 +1,49 @@
 import Swatch from "./Swatch"
 import styled from "styled-components";
+import { motion } from "framer-motion";
+import { useEffect, useState } from "react";
+
+const AnimationVariants = {
+    initial: {
+        scale: 0.75,
+        y: 50
+    },
+    animate: (index) => ({ 
+        scale: 1,
+        y: 0,
+        transition: {
+            duration: 0.05 * index,
+            type: 'spring' 
+        }
+    })
+}
 
 export default function SwatchGroupView(props) {
+    const [key, setKey] = useState(0);
+
+    useEffect(() => {
+        setKey(prevKey => prevKey + 1);
+    }, [props.delegate]);
+
     if (!props.model) return
     return (
         <Container>
             <Title>{props.model.semantic}</Title>
             <Main className="ScaleView">
                 {props.model.swatches.map((model, index) => {
-                        return <Swatch key={index} model={model} delegate={props.delegate} />
+                    return (
+                        <motion.div
+                            key={`${key}-${index}`}
+                            variants={AnimationVariants}
+                            initial="initial"
+                            animate="animate"
+                            custom={index}
+                        >
+                            <Swatch model={model} delegate={props.delegate} />
+                        </motion.div>
+                    );
                 })}
-        </Main>
+            </Main>
         </Container>
     )
 }
@@ -33,4 +66,4 @@ const Main = styled.div`
     width: 100%
     flex: 1;
     background: white;
-    ` 
+    `
