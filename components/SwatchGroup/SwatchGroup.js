df83a13f1458af5fed6817c407607c9c0faac491
Frank Stallone <frank@stallone.dev>
2024-12-29 10:21:41 -0500

    wip: Animation functionality for Swatches in place, still need to finess the actual animations

--- components/SwatchGroup/SwatchGroup.js
+++ components/SwatchGroup/SwatchGroup.js
@@ -1,10 +1,13 @@
-import { useEffect, useRef } from 'react';
-import { motion } from 'framer-motion';
+import { AnimatePresence, motion } from 'framer-motion';
 import styled from 'styled-components';
 import { optimizations } from '../../models/OptimizationModel.js';
 import Swatch from './Swatch';
 
 const AnimationVariants = {
+  initial: {
+    scale: 0,
+    y: 50,
+  },
   animateIn: {
     scale: 1,
     y: 0,
@@ -13,7 +16,6 @@ const AnimationVariants = {
     scale: 0,
     y: 50,
   },
-  noAnimation: {},
 };
 
 export default function SwatchGroupView(props) {
@@ -21,44 +23,27 @@ export default function SwatchGroupView(props) {
 
   const optimization = optimizations.find((item) => item.name === props.delegate.optimization);
 
-  // Ref to store the previous optimization object
-  const previousOptimization = useRef(null);
-
-  useEffect(() => {
-    if (previousOptimization.current && previousOptimization.current.name !== optimization?.name) {
-      console.log(
-        'Optimization changed from',
-        previousOptimization.current.name,
-        'to',
-        optimization?.name
-      );
-    }
-
-    previousOptimization.current = optimization; // Update the ref after comparison
-  }, [optimization]);
-
   return (
     <Container>
       <Title>{props.model.semantic}</Title>
       <Main className="ScaleView">
         {props.model.swatches.map((model, index) => {
-          // Compare weights for the current index
-          const previousWeight = previousOptimization.current?.values[index]?.weight;
           const currentWeight = optimization?.values[index]?.weight;
 
-          // Determine the correct animation
-          const animate = !previousOptimization.current
-            ? AnimationVariants.noAnimation
-            : previousWeight === undefined && currentWeight !== undefined
-              ? AnimationVariants.animateIn
-              : previousWeight !== undefined && currentWeight === undefined
-                ? AnimationVariants.animateOut
-                : AnimationVariants.noAnimation;
-
           return (
-            <motion.div key={index} animate={animate} variants={AnimationVariants} custom={index}>
-              <Swatch model={model} delegate={props.delegate} />
-            </motion.div>
+            <AnimatePresence initial={false}>
+              {currentWeight && (
+                <motion.div
+                  key={index}
+                  initial={'initial'}
+                  animate={'animateIn'}
+                  exit={'animateOut'}
+                  variants={AnimationVariants}
+                >
+                  <Swatch model={model} delegate={props.delegate} />
+                </motion.div>
+              )}
+            </AnimatePresence>
           );
         })}
       </Main>
