ba28840d5e75b9fa06f6f437111eccde22fa742a
Frank Stallone <frank@stallone.dev>
2025-01-05 09:29:25 -0500

    refactor: prettier save on existing NavBar file

--- components/NavBar/NavBar.tsx
+++ components/NavBar/NavBar.tsx
@@ -1,30 +1,32 @@
-import Image from "next/image";
-import styled from "styled-components";
+import Image from 'next/image';
+import { IconDownload } from '@tabler/icons-react';
+import styled from 'styled-components';
 import { Button } from '@mantine/core';
-import { IconDownload } from "@tabler/icons-react";
-import NavBarMenu from "./NavBarMenu";
+import NavBarMenu from './NavBarMenu';
 
 export default function NavBar(props: any) {
-  if (!props.model) return
+  if (!props.model) return;
   return (
     <Wrapper>
-      <Image
-        src="images/logo.svg"
-        width={130}
-        height={34}
-        alt="PrismColor Logo"
-      />
+      <Image src="images/logo.svg" width={130} height={34} alt="PrismColor Logo" />
       <ButtonGroup>
         <NavBarMenu />
-        <Button rightSection={<IconDownload size={18} />} size="xs" color="#0070c1" onClick={() => downloadAction(props)}>Download</Button>
+        <Button
+          rightSection={<IconDownload size={18} />}
+          size="xs"
+          color="#0070c1"
+          onClick={() => downloadAction(props)}
+        >
+          Download
+        </Button>
       </ButtonGroup>
     </Wrapper>
-  )
+  );
 }
 
 function downloadAction(props: any) {
-  const {model, delegate} = props
-  const json = JSON.stringify({optimization:delegate.optimization, ...model}, null, 4);
+  const { model, delegate } = props;
+  const json = JSON.stringify({ optimization: delegate.optimization, ...model }, null, 4);
   const blob = new Blob([json], { type: 'application/json' });
   downloadBlob(blob);
 }
@@ -34,7 +36,7 @@ function downloadBlob(blob: any, name = 'prismColorPalette.json') {
   const blobUrl = URL.createObjectURL(blob);
 
   // Create a link element
-  const link = document.createElement("a");
+  const link = document.createElement('a');
 
   // Set link's href to point to the Blob URL
   link.href = blobUrl;
@@ -46,10 +48,10 @@ function downloadBlob(blob: any, name = 'prismColorPalette.json') {
   // Dispatch click event on the link
   // This is necessary as link.click() does not work on the latest firefox
   link.dispatchEvent(
-    new MouseEvent('click', { 
-      bubbles: true, 
-      cancelable: true, 
-      view: window 
+    new MouseEvent('click', {
+      bubbles: true,
+      cancelable: true,
+      view: window,
     })
   );
 
@@ -61,7 +63,7 @@ const ButtonGroup = styled.div`
   display: flex;
   align-items: center;
   gap: 4px;
-`
+`;
 
 const Wrapper = styled.div`
   display: flex;
@@ -69,8 +71,8 @@ const Wrapper = styled.div`
   justify-content: space-between;
   width: 100%;
   height: 56px;
-  border-bottom:1px solid #e3e3e3;
+  border-bottom: 1px solid #e3e3e3;
   background: #ffffff;
   padding-left: 24px;
   padding-right: 24px;
-`
+`;
