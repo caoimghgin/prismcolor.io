3969f2fd3b23a9d494484032c9b97fe5f70400e0
Frank Stallone <frank@stallone.dev>
2025-01-11 15:19:23 -0500

    feat: Adding Million to app

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,15 @@
+import MillionLint from '@million/lint';
 import bundleAnalyzer from '@next/bundle-analyzer';
 
 const withBundleAnalyzer = bundleAnalyzer({
   enabled: process.env.ANALYZE === 'true',
 });
 
-export default withBundleAnalyzer({
+export default MillionLint.next({
+  enabled: true
+})(withBundleAnalyzer({
   reactStrictMode: false,
   eslint: {
     ignoreDuringBuilds: true,
   },
-});
+}));
