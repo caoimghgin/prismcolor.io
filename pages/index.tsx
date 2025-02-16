6029b2b8fd167e67e2b66e8a0ec3870fe266dc89
Kevin Muldoon <caoimghgin@gmail.com>
2025-02-16 10:47:09 -0500

    Independent Section Scrolls
    Also, leftNav and main now fills the remaining vertical space in modes 'create' and 'View'.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,10 +1,18 @@
+import { Button, Group } from '@mantine/core';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
+import { useRouter } from 'next/navigation';
+
 export default function HomePage() {
+  const router = useRouter();
   return (
     <>
       <Welcome />
       <ColorSchemeToggle />
+      <Group justify="center" mt="xl">
+        <Button onClick={() => router.push('/create')}>Go to Create Page</Button>
+      </Group>
     </>
   );
 }
+
